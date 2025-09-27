41072e7dbfcc0a25db4bff55229c9fe3be81347c
Gianluca Mardente <gianluca.mardente@gmail.com>
2025-09-27 07:34:59 +0200

    (feat) add metrics
    ```
    	deletedResourceCounter = prometheus.NewCounterVec(
    		prometheus.CounterOpts{
    			Namespace: os.Getenv("NAMESPACE"),
    			Name:      deletedCounterName,
    			Help:      deletedCounterHelp,
    		},
    		[]string{"cleaner_instance", "resource_apiversion", "resource_type"},
    	)
    
    	updatedResourceCounter = prometheus.NewCounterVec(
    		prometheus.CounterOpts{
    			Namespace: os.Getenv("NAMESPACE"),
    			Name:      updatedCounterName,
    			Help:      updatedCounterHelp,
    		},
    		[]string{"cleaner_instance", "resource_apiversion", "resource_type"},
    	)
    ```

---
+++ internal/controller/executor/metrics.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2025. projectsveltos.io. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package executor
+
+import (
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+// --- Metric Constants ---
+
+const (
+	deletedCounterName = "k8s_cleaner_deleted_resources_total"
+	deletedCounterHelp = "The cumulative count of resources successfully deleted by the cleaner."
+
+	updatedCounterName = "k8s_cleaner_updated_resources_total"
+	updatedCounterHelp = "The cumulative count of resources successfully updated by the cleaner."
+)
+
+var (
+	deletedResourceCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: os.Getenv("NAMESPACE"),
+			Name:      deletedCounterName,
+			Help:      deletedCounterHelp,
+		},
+		[]string{"cleaner_instance", "resource_apiversion", "resource_type"},
+	)
+
+	updatedResourceCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: os.Getenv("NAMESPACE"),
+			Name:      updatedCounterName,
+			Help:      updatedCounterHelp,
+		},
+		[]string{"cleaner_instance", "resource_apiversion", "resource_type"},
+	)
+)
+
+//nolint:gochecknoinits // forced pattern for prometheus and controller-runtime
+func init() {
+	// Register custom metrics with the controller-runtime's global registry.
+	// This ensures it is exported alongside default controller-runtime metrics.
+	metrics.Registry.MustRegister(deletedResourceCounter, updatedResourceCounter)
+}
+
+// getDeletedResourcesCounterVec returns the singleton CounterVec instance.
+func getDeletedResourcesCounterVec() *prometheus.CounterVec {
+	return deletedResourceCounter
+}
+
+func reportDeletionEvent(cleanerName, resourceAPIVersion, resourceKind string) {
+	// Get the global CounterVec instance.
+	counterVec := getDeletedResourcesCounterVec()
+
+	// Increment the counter for the specific cleaner instance and resource kind.
+	counterVec.WithLabelValues(cleanerName, resourceAPIVersion, resourceKind).Inc()
+}
+
+// getUpdatedResourcesCounterVec returns the singleton CounterVec instance.
+func getUpdatedResourcesCounterVec() *prometheus.CounterVec {
+	return deletedResourceCounter
+}
+
+func reportUpdateEvent(cleanerName, resourceAPIVersion, resourceKind string) {
+	// Get the global CounterVec instance.
+	counterVec := getUpdatedResourcesCounterVec()
+
+	// Increment the counter for the specific cleaner instance and resource kind.
+	counterVec.WithLabelValues(cleanerName, resourceAPIVersion, resourceKind).Inc()
+}
