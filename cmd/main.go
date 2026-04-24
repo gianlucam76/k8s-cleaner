0b510387138b28f08c4ed5b064a6fde72186fb31
Pierluigi Lenoci <pierluigilenoci@gmail.com>
2026-04-24 12:35:25 +0200

    fix: add nolint explanation to satisfy nolintlint
    The nolintlint linter requires that //nolint directives include an
    explanation comment. Add the reason for each errcheck suppression.
    
    Signed-off-by: Pierluigi Lenoci <pierluigilenoci@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -84,8 +84,8 @@ func main() {
 	// Opt into the new klog behavior so that -stderrthreshold is honored even
 	// when -logtostderr=true (the default).
 	// Ref: kubernetes/klog#212, kubernetes/klog#432
-	flag.Set("legacy_stderr_threshold_behavior", "false") //nolint:errcheck
-	flag.Set("stderrthreshold", "INFO")                   //nolint:errcheck
+	flag.Set("legacy_stderr_threshold_behavior", "false") //nolint:errcheck // flag is registered by klog.InitFlags above
+	flag.Set("stderrthreshold", "INFO")                   //nolint:errcheck // flag is registered by klog.InitFlags above
 
 	initFlags(pflag.CommandLine)
 	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
